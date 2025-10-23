31bf0681b267104b5a7d8ff5802d506d5f4b9398
Yongchao Gan <yongchao.Gan@desaysv.com>
2025-10-23 16:22:50 +0800

    add heatmap and nadaraya_waston

--- ch10_attention/attention_heatmap_1.py
+++ ch10_attention/attention_heatmap_1.py
@@ -4,7 +4,7 @@ from torch.nn import functional as F
 
 #@save
 def show_heatmaps(matrices, xlabel, ylabel, titles=None, figsize=(2.5, 2.5),
-                  cmap='Reds'):
+                  cmap='Reds', save_path=None):
     """显示矩阵热图"""
     d2l.use_svg_display()
     num_rows, num_cols = matrices.shape[0], matrices.shape[1]
@@ -21,14 +21,17 @@ def show_heatmaps(matrices, xlabel, ylabel, titles=None, figsize=(2.5, 2.5),
                 ax.set_title(titles[j])
     fig.colorbar(pcm, ax=axes, shrink=0.6)
 
+    if save_path:
+        d2l.plt.savefig(save_path)
+
 
 # 仅当查询和键相同时，注意力权重为1，否则为0
 attention_weights = torch.eye(10).reshape((1, 1, 10, 10))
-show_heatmaps(attention_weights, xlabel='Keys', ylabel='Queries')
+show_heatmaps(attention_weights, xlabel='Keys', ylabel='Queries',save_path='./heatmap_test1.png')
 
 
 # 随机生成一个10*10矩阵并使用softmax运算保证每行都是有效的概率分布，然后可视化输出注意力权重
 X = torch.rand((10,10))
 X = F.softmax(X,dim=1)
 X = X.reshape((1,1,10,10))
-show_heatmaps(X,xlabel='Columns',ylabel='Rows')
+show_heatmaps(X,xlabel='Columns',ylabel='Rows',save_path='./heatmap_test2.png')
