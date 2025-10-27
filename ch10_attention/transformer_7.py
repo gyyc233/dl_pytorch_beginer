891150fe1bcae98d3cba389bc1f5b2ef13a021bb
Yongchao Gan <yongchao.Gan@desaysv.com>
2025-10-27 21:30:21 +0800

    update transformer

---
+++ ch10_attention/transformer_7.py
@@ -0,0 +1,38 @@
+import math
+import pandas as pd
+import torch
+from torch import nn
+from d2l import torch as d2l
+
+import sys
+sys.path.append('./ch10_attention') # based on dl_pytorch_beginer/
+
+import attention_scoring_function_3 as attention_scoring_function_3
+
+
+#@save
+class PositionWiseFFN(nn.Module):
+    """基于位置的前馈网络"""
+    def __init__(self, ffn_num_input, ffn_num_hiddens, ffn_num_outputs,
+                 **kwargs):
+        super(PositionWiseFFN, self).__init__(**kwargs)
+        self.dense1 = nn.Linear(ffn_num_input, ffn_num_hiddens)
+        self.relu = nn.ReLU()
+        self.dense2 = nn.Linear(ffn_num_hiddens, ffn_num_outputs)
+
+    def forward(self, X):
+        return self.dense2(self.relu(self.dense1(X)))
+
+
+# 残差连接与层规范化
+#@save
+class AddNorm(nn.Module):
+    """残差连接后进行层规范化"""
+    def __init__(self, normalized_shape, dropout, **kwargs):
+        super(AddNorm, self).__init__(**kwargs)
+        self.dropout = nn.Dropout(dropout)
+        self.ln = nn.LayerNorm(normalized_shape)
+
+    def forward(self, X, Y):
+        return self.ln(self.dropout(Y) + X)
+    
